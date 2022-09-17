dc41dd3e7b787aa27d787089779f1a323819d5be
Gurmukhnishan Singh <nishansaiyan@gmail.com>
2022-09-18 03:24:41 +0530

    added few more commands

--- script/Profile.js
+++ script/Profile.js
@@ -7,11 +7,15 @@ $(function() {
 
 
 Use following command for getting details about me
--------------------------------------------
-| [[b;#0000FF;]Command] |          [[b;#0000FF;]Description]          |
--------------------------------------------
-| [[b;#0000FF;]whoami]  | To read something about me    |
--------------------------------------------
+--------------------------------------------
+| [[b;#0000FF;]Command]  |          [[b;#0000FF;]Description]          |
+--------------------------------------------
+| [[b;#0000FF;]whoami]   | To read something about me    |
+| [[b;#0000FF;]about]    | To read info about this site  |
+| [[b;#0000FF;]blogs]    | Read about my blogs           |
+| [[b;#0000FF;]Experience| Read about my work experience |
+| [[b;#0000FF;]Projects  | Read about project i have done|
+--------------------------------------------
 `
     });
  });
