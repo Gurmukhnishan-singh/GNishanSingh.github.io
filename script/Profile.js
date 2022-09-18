9eab6f688ca8766a65caf9c6f9233453877f966d
Gurmukhnishan Singh <nishansaiyan@gmail.com>
2022-09-18 06:46:31 +0530

    added images

--- script/Profile.js
+++ script/Profile.js
@@ -4,6 +4,7 @@ $(function() {
 ----------------------------------------------------------------------------
 | [[b;#0000FF;]G] | [[b;#0000FF;]U] | [[b;#0000FF;]R] | [[b;#0000FF;]M] | [[b;#0000FF;]U] | [[b;#0000FF;]K] | [[b;#0000FF;]H] | [[b;#0000FF;]N] | [[b;#0000FF;]I] | [[b;#0000FF;]S] | [[b;#0000FF;]H] | [[b;#0000FF;]A] | [[b;#0000FF;]N] |  | [[b;#0000FF;]S] | [[b;#0000FF;]I] | [[b;#0000FF;]N] | [[b;#0000FF;]G] | [[b;#0000FF;]H] |
 ----------------------------------------------------------------------------
+[[@;;;terminal img]images/iconphoto.jpeg]
 
 
 Use following command for getting details about me
@@ -17,6 +18,7 @@ Use following command for getting details about me
      | [[b;#0000FF;]Projects]  | Read about project i have done|
      ---------------------------------------------
 
-`
+`,
+    prompt:'g_nishan_singh >'
     });
  });
